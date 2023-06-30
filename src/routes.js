8831bcf21a78dd75291053b995fc354daf5e03ea
Almeidaaaaaa <deboralmeidavieira@gmail.com>
2023-06-30 11:52:06 -0300

    Alteração no nome das raizes

--- src/routes.js
+++ src/routes.js
@@ -1,12 +1,12 @@
 const express = require('express');
-const controllers = require('./controllers/recuros');
+const controllers = require('./controllers/recursos');
 
 const routes = express.Router();
 
 routes.get('/', controllers.raiz);
-routes.get('rota2', controllers.raiz);
-routes.get('rota3', controllers.raiz);
-routes.get('rota4', controllers.raiz);
-routes.get('rota5', controllers.raiz);
+routes.get('/r2', controllers.raiz);
+routes.get('/r3', controllers.raiz);
+routes.get('/r4', controllers.raiz);
+routes.get('/r5', controllers.raiz);
 
 module.exports = routes;
