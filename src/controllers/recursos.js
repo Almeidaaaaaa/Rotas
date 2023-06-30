6975f523233959f3971aca0b3ed8c8679a2a2ba7
Almeidaaaaaa <deboralmeidavieira@gmail.com>
2023-06-30 11:38:12 -0300

    Criação das outras raizes

--- src/controllers/recursos.js
+++ src/controllers/recursos.js
@@ -2,5 +2,17 @@
 module.exports = {
     async raiz(req,res){
       return res.send('Respota da raiz');
-    }
+    },
+    async rota2(req,res){
+        return res.send('Resposta da 2 raiz');
+      },
+      async rota3(req,res){
+        return res.send('Resposta da 3 raiz');
+      },
+      async rota4(req,res){
+        return res.send('Resposta da 4 raiz');
+      },
+      async rota5(req,res){
+        return res.send('Resposta da 5 raiz');
+      },
 };
--- src/routes.js
+++ src/routes.js
@@ -4,5 +4,9 @@ const controllers = require('./controllers/recuros');
 const routes = express.Router();
 
 routes.get('/', controllers.raiz);
+routes.get('rota2', controllers.raiz);
+routes.get('rota3', controllers.raiz);
+routes.get('rota4', controllers.raiz);
+routes.get('rota5', controllers.raiz);
 
 module.exports = routes;
