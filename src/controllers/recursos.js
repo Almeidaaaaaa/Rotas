5dd41ee15657eacbfe03aa8c596fe65df81edfb5
Almeidaaaaaa <deboralmeidavieira@gmail.com>
2023-06-30 11:53:25 -0300

    Alteração no recurso

--- src/controllers/recursos.js
+++ src/controllers/recursos.js
@@ -3,16 +3,16 @@ module.exports = {
     async raiz(req,res){
       return res.send('Respota da raiz');
     },
-    async rota2(req,res){
+    async r2(req,res){
         return res.send('Resposta da 2 raiz');
       },
-      async rota3(req,res){
+      async r3(req,res){
         return res.send('Resposta da 3 raiz');
       },
-      async rota4(req,res){
+      async r4(req,res){
         return res.send('Resposta da 4 raiz');
       },
-      async rota5(req,res){
+      async r5(req,res){
         return res.send('Resposta da 5 raiz');
       },
 };
